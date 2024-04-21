035e69c2812feb39f7d6e8e5cb9722ceb4cf8bf8
acsawall <acsawall@gmail.com>
2024-04-21 14:26:26 -0500

    Reversable tiff -> Tensor -> waveform works

--- rainspec_dataset.py
+++ rainspec_dataset.py
@@ -18,12 +18,13 @@ from tqdm import tqdm
 from audio_to_spec import get_melspec_db, spec_to_img, load_audio_from_tiff
 from skimage import io, transform
 import matplotlib.pyplot as plt
+from torchvision.datasets import VisionDataset
 
 class RainSpecDataset(Dataset):
     def __init__(
             self,
             image_dir,
-            transform = None
+            transform=None
     ):
         self.image_dir = image_dir
         self.transform = transform
@@ -31,6 +32,8 @@ class RainSpecDataset(Dataset):
         for file in tqdm(os.listdir(image_dir)):
             img_path = os.path.join(self.image_dir, file)
             image = Image.open(img_path)
+            image = ToImage()(image)
+            image = ToDtype(torch.float32, scale=True)(image)
             self.data.append(image)
 
     def __len__(self):
@@ -38,12 +41,12 @@ class RainSpecDataset(Dataset):
 
     def __getitem__(self, index):
         image = self.data[index]
+        #image *= 50
         if self.transform:
             image = self.transform(image)
-        #img_tensor = torchvision.transforms.ToTensor()(image)
-        #img_tensor *= 50        # TODO should this be done for training or sampling?
-        #plt.pcolormesh(image.cpu().data.squeeze())
-        #plt.show()
+
+        plt.pcolormesh(image.squeeze())
+        plt.show()
         return image, 0
 
 
@@ -52,14 +55,14 @@ if __name__ == "__main__":
     rsd = RainSpecDataset(dir, torchvision.transforms.Compose(
                                         [
                                             Resize(256),
-                                            Compose([ToImage(), ToDtype(torch.float32, scale=True)]),
                                             #Normalize((0.5,), (0.5,))
                                         ]
                                     ))
 
     m = rsd[0][0]
-    img = np.array(m.squeeze())
-    img *= 50
+    img = np.array(m.squeeze()) * 50
+    plt.imshow(img)
+    plt.show()
     aud = librosa.feature.inverse.mel_to_audio(img, sr=22050, n_fft=2048, hop_length=512)
     plt.plot(aud)
     plt.show()
